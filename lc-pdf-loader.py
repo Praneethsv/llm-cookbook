69fcc1bacdb2620b1a3aa4ae3112b768a580d3d3
praneeth-sarafin <sarafin-ml@outlook.com>
2024-11-14 11:39:46 -0500

    initial commit

---
+++ lc-pdf-loader.py
@@ -0,0 +1,15 @@
+from langchain_community.document_loaders import UnstructuredPDFLoader
+
+text_loader = UnstructuredPDFLoader(image_extractor=True, table_extractor=True, 
+                                    file_path='/home/sv/Downloads/Nexus-Downloads/BackUp-Downloads/Venkata_Praneeth_CV_Updated.pdf')
+
+pdf_data = text_loader.load()
+
+
+for page in pdf_data:
+
+    tables = page.tables
+
+    images = page.images
+
+print(tables)
